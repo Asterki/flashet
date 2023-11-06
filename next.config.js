7775989f441e70a8c532c0406d1414e60cdc6213
Asterki <asterki.dev@proton.me>
2023-11-06 09:25:16 -0600

    Fix: added responsiveness to some pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,45 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  images: {
+      remotePatterns: [
+          {
+              protocol: "https",
+              hostname: "lh3.googleusercontent.com",
+              port: "",
+              pathname: "/a/**",
+          },
+      ],
+  },
+
+  async rewrites() {
+      return [
+          // Main
+          {
+              source: "/home",
+              destination: "/main/home",
+          },
+          {
+              source: "/",
+              destination: "/main",
+          },
+          {
+              source: "/error",
+              destination: "/main/error",
+          },
+
+          // Accounts
+          {
+              source: "/signin",
+              destination: "/auth/signin",
+          },
+          {
+              source: "/signout",
+              destination: "/auth/signout",
+          },
+      ];
+  },
+};
+
+module.exports = nextConfig;
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,25 +1,45 @@
 import * as React from "react";
-import { motion } from "framer-motion";
-
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
+import Link from "next/link";
+import { motion } from "framer-motion";
+
 const Home = () => {
-    const router = useRouter()
+    const router = useRouter();
+
+    const { data: session } = useSession({
+        required: false,
+    });
 
     return (
         <div className="text-white bg-gradient-to-tr from-blue-950 to-cyan-800">
-            <header className="absolute flex items-center justify-between w-full p-16">
-                <div className="text-[40px]">
-                    Flashet
-                </div>
+            <header className="absolute flex items-center justify-between w-full p-4 md:p-16">
+                <div className="text-[40px]">Flashet</div>
                 <div className="flex items-center">
-                    <a href="/about" className="mx-2">About</a>
-                    <a href="ejwq" className="mx-2">Open Source</a>
-                    <a href="ejwq" className="mx-2">Login</a>
+                    <Link href="/about" className="mx-2">
+                        About
+                    </Link>
+                    <Link
+                        href="https://github.com/Asterki/flashet"
+                        className="mx-2"
+                    >
+                        Open Source
+                    </Link>
+                    {session && (
+                        <Link href="/auth/signout" className="mx-2">
+                            Logged in as {session.user?.name}
+                        </Link>
+                    )}
+                    {!session && (
+                        <Link href="/auth/sigin" className="mx-2">
+                            Login
+                        </Link>
+                    )}
                 </div>
             </header>
 
-            <main className="min-h-screen flex flex-col justify-center items-center p-24">
+            <main className="min-h-screen flex flex-col justify-center items-center p-4 md:p-24">
                 <div className="flex flex-col-reverse lg:flex-row items-center justify-between lg:w-9/12 w-full">
                     <div className="lg:w-5/12 w-full text-center lg:text-left">
                         <p className="text-[50px] font-black mb-12">
@@ -34,9 +54,22 @@ const Home = () => {
                         </p>
 
                         <div className="mt-10">
-                            <button onClick={() => router.push("/app")} className="bg-white/10 hover:bg-white/20 transition-all p-4 rounded-lg shadow-md">
-                                Go to your dashboard
-                            </button>
+                            {session && (
+                                <button
+                                    onClick={() => router.push("/app")}
+                                    className="bg-white/10 hover:bg-white/20 transition-all w-full md:w-5/12 p-4 rounded-lg shadow-md"
+                                >
+                                    Go to your dashboard
+                                </button>
+                            )}
+                            {!session && (
+                                <button
+                                    onClick={() => router.push("/app")}
+                                    className="bg-white/10 hover:bg-white/20 transition-all w-5/12 p-4 rounded-lg shadow-md"
+                                >
+                                    Login
+                                </button>
+                            )}
                         </div>
                     </div>
                     <div className="lg:w-5/12 lg:flex hidden justify-end items-end">
