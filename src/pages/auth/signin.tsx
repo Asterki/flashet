9f610462e6030912746e2243aa58da96275f06fe
Asterki <asterki.dev@proton.me>
2023-11-06 09:22:46 -0600

    Feat: Account Pages

---
+++ src/pages/auth/signin.tsx
@@ -0,0 +1,57 @@
+import type {
+    GetServerSidePropsContext,
+    InferGetServerSidePropsType,
+} from "next";
+import { getProviders, signIn } from "next-auth/react";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../api/auth/[...nextauth]";
+
+export default function SignIn({
+    providers,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    return (
+        <div className="text-white bg-gradient-to-tr from-blue-950 to-cyan-800">
+            <main className="min-h-screen flex flex-col justify-center items-center p-24">
+                <section>
+                    <div className="my-4 text-gray-100 text-center">
+                        <p className="text-[50px] font-black mb-4">
+                            Sign in to Flashet
+                        </p>
+                        <p>Please select a provider</p>
+                    </div>
+
+                    <div className="text-center">
+                        {Object.values(providers).map((provider) => (
+                            <div key={provider.name}>
+                                <button
+                                    className="bg-white/10 hover:bg-white/20 transition-all w-5/12 p-4 rounded-lg shadow-md"
+                                    onClick={() => signIn(provider.id)}
+                                >
+                                    Sign in with {provider.name}
+                                </button>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+            </main>
+        </div>
+    );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+    const session = await getServerSession(
+        context.req,
+        context.res,
+        authOptions
+    );
+
+    if (session) {
+        return { redirect: { destination: "/" } };
+    }
+
+    const providers = await getProviders();
+
+    return {
+        props: { providers: providers ?? [] },
+    };
+}
---
+++ src/pages/auth/signout.tsx
@@ -0,0 +1,48 @@
+import { useRouter } from "next/router";
+import { useSession, signOut } from "next-auth/react";
+
+const SignOut = () => {
+    const router = useRouter();
+    const { data: session, status } = useSession({
+        required: true,
+        onUnauthenticated() {
+            router.push("/auth/signin");
+        },
+    });
+
+    return (
+        <div className="text-white bg-gradient-to-tr from-blue-950 to-cyan-800">
+            <main className="min-h-screen flex flex-col justify-center items-center p-12">
+                <section className="md:w-4/12 w-full">
+                    <div className="my-4 text-gray-100 text-center">
+                        <h1 className="text-3xl">Sign Out</h1>
+                        <p>Are you sure you want to sign out?</p>
+                    </div>
+
+                    <button
+                        className="bg-white/20 hover:bg-red-400 w-full shadow-md rounded-md p-4 transition-all"
+                        onClick={() =>
+                            signOut({ redirect: true, callbackUrl: "/" })
+                        }
+                    >
+                        Sign Out
+                    </button>
+
+                    <br />
+                    <br />
+
+                    <button
+                        className="bg-white/20 hover:bg-sky-400 w-full shadow-md rounded-md p-4 transition-all"
+                        onClick={() => router.push("/auth/signin")}
+                    >
+                        Cancel
+                    </button>
+                </section>
+
+                <section className="absolute bottom-2 text-gray-300">Logged in as {session?.user?.email}</section>
+            </main>
+        </div>
+    );
+};
+
+export default SignOut;
