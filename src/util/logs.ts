9603ef4442ffb57599b24f9dfe5f335888314aed
Asterki <asterki.dev@proton.me>
2023-11-07 10:54:01 -0600

    Fix: Logs not showing properly on screen

--- src/util/logs.ts
+++ src/util/logs.ts
@@ -1,19 +1,19 @@
 import chalk from "chalk";
 
 const logSuccess = (message: any) => {
-    console.log(`${chalk.greenBright("✓")} ${message}`);
+    console.log(` ${chalk.bold(chalk.greenBright("✓"))} ${message}`);
 };
 
 const logWarning = (message: any) => {
-    console.log(`${chalk.yellowBright("⚠")} ${message}`);
+    console.log(` ${chalk.bold(chalk.yellowBright("⚠"))} ${message}`);
 };
 
 const logError = (message: any) => {
-    console.log(`${chalk.redBright("⨯")} ${message}`);
+    console.log(` ${chalk.bold(chalk.redBright("⨯"))} ${message}`);
 };
 
 const logLog = (message: any) => {
-    console.log(`${chalk.whiteBright("○")} ${message}`);
+    console.log(` ${chalk.bold(chalk.whiteBright("○"))} ${message}`);
 };
 
 export { logSuccess, logWarning, logError, logLog };
