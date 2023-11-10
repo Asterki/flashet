241098c09674c2091a71a060b371f0a76c87c893
Asterki <asterki.dev@proton.me>
2023-11-10 16:00:17 -0600

    Feat: Russian translations

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -6,7 +6,7 @@
 module.exports = {
     i18n: {
         defaultLocale: "en",
-        locales: ["en", "de"],
+        locales: ["en", "de", "fr", "es", "ru"],
     },
     /** To avoid issues when deploying to some paas (vercel...) */
     localePath: typeof window === "undefined" ? require("path").resolve("./public/locales") : "/locales",
