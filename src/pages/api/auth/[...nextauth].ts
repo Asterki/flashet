fde0e8ad2972735021139f4d2edd15a674bb34f4
Asterki <asterki.dev@proton.me>
2023-11-18 15:14:50 -0600

    Feat: Discord login support

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import DiscordProvider from "next-auth/providers/discord";
 
 import NextAuth, { AuthOptions } from "next-auth";
 
@@ -39,6 +40,10 @@ export const authOptions: AuthOptions = {
         GitHubProvider({
             clientId: process.env.GITHUB_CLIENT_ID as string,
             clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        }),
+        DiscordProvider({
+            clientId: process.env.DISCORD_CLIENT_ID as string,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
         }),
     ],
     pages: {
